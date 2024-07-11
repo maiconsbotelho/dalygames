c6d3db340babbb2cd44c772665a93e7d5b51ee18
Maicon Botelho <maicon.sbotelho@hotmail.com>
2024-07-11 17:33:09 -0300

    find: add Head component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,26 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { LiaGamepadSolid } from 'react-icons/lia';
+import Logo from '../../../public/logo.svg';
+
+// Componente de cabeçalho
 const Header = () => {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className="h-28 w-full bg-slate-100 px-2 text-black">
+      <div className="mx-auto flex h-28 max-w-screen-xl items-center justify-center sm:justify-between">
+        <nav className="flex items-center justify-center gap-4">
+          <Link href="/">
+            <Image src={Logo} alt="logo do site DalyGames" quality={100} priority={true} className="w-full" />
+          </Link>
+          <Link href="/">Games</Link>
+          <Link href="/profile">Perfil</Link>
+        </nav>
+        <div className="hidden sm:flex">
+          <Link href="/profile">
+            <LiaGamepadSolid size={34} color="#475569" />
+          </Link>
+        </div>
+      </div>
     </header>
   );
 };
